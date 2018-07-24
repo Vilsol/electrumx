e4345b3d8252316146c6d001066f729b7d3871cb
Vilsol <Vilsol@users.noreply.github.com>
2018-07-24 20:48:02 +0300

    Update history.py

--- server/history.py
+++ server/history.py
@@ -118,7 +118,7 @@ class History(object):
 
     def flush(self):
         self.flush_count += 1
-        print(self.flush_count)
+        self.logger.info(f'flush count: {self.flush_count:,d}')
         flush_id = pack('>H', self.flush_count)
         unflushed = self.unflushed
 
